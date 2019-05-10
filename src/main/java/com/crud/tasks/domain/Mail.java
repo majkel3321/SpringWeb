a569b26b7f0326ac12c2979841e7dbe6f7702d88
majkel3321 <majkel3321@gmail.com>
2019-05-10 16:00:30 +0200

    emailServicee

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -11,6 +11,7 @@ public class Mail {
     private String message;
     private String toCc;
 
+
     public Mail(String mailTo, String subject, String message) {
         this.mailTo = mailTo;
         this.subject = subject;
