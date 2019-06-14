de2d603ecf821f49344bb323bc721e51cb3036ad
majkel3321 <majkel3321@gmail.com>
2019-06-14 12:28:13 +0200

    DbService Test

--- src/test/java/com/crud/tasks/service/DbServiceTest.java
+++ src/test/java/com/crud/tasks/service/DbServiceTest.java
@@ -1,15 +1,10 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.domain.Task;
-import com.crud.tasks.repository.TaskRepository;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,7 +12,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DbServiceTest {
@@ -28,10 +25,10 @@ public class DbServiceTest {
     private List<Task> taskList;
 
     @Before
-    public void createAndSaveTasks(){
-        Task task1 = new Task(1L,"Task1","Task1 content");
-        Task task2 = new Task(1L,"Task2","Task2 content");
-        Task task3 = new Task(1L,"Task3","Task3 content");
+    public void createAndSaveTasks() {
+        Task task1 = new Task(1L, "Task1", "Task1 content");
+        Task task2 = new Task(1L, "Task2", "Task2 content");
+        Task task3 = new Task(1L, "Task3", "Task3 content");
 
         dbService.saveTask(task1);
         dbService.saveTask(task2);
@@ -41,10 +38,38 @@ public class DbServiceTest {
     }
 
     @After
-    public void cleanUp(){
+    public void cleanUp() {
         dbService.deleteTask(taskList.get(0).getId());
         dbService.deleteTask(taskList.get(1).getId());
         dbService.deleteTask(taskList.get(2).getId());
+    }
+
+    @Test
+    public void saveTask() {
+        //Given
+        Task task = new Task(4L, "Task4", "Task4 content");
+
+        //When
+        dbService.saveTask(task);
+
+        //Then
+        assertEquals(4, dbService.getAllTasks().size());
+
+        //CleanUp
+        dbService.deleteTask(dbService.getAllTasks().get(3).getId());
+    }
+
+    @Test
+    public void deleteTask() {
+        //Given
+        Task task = new Task(4L, "Task4", "Task4 content");
+        dbService.saveTask(task);
+
+        //When
+        dbService.deleteTask(dbService.getAllTasks().get(3).getId());
+
+        //Then
+        assertEquals(3, dbService.getAllTasks().size());
     }
 
     @Test
@@ -53,17 +78,17 @@ public class DbServiceTest {
         List<Task> taskList = dbService.getAllTasks();
 
         //Then
-        assertEquals(3,taskList.size());
-        assertEquals("Task2",taskList.get(1).getTitle());
+        assertEquals(3, taskList.size());
+        assertEquals("Task2", taskList.get(1).getTitle());
     }
 
     @Test
-    public void getTask(){
+    public void getTask() {
         //Given&When
         Optional<Task> task = dbService.getTask(taskList.get(0).getId());
 
         //Then
         assertTrue(task.isPresent());
-        assertEquals("Task1",task.get().getTitle());
+        assertEquals("Task1", task.get().getTitle());
     }
 }
